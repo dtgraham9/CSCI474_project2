20da2d4379a13ce61fa9a8f9b194c6c5b35e53a3
Paul Alexander Howell <phowell@lab10.cs.ndsu.nodak.edu>
2019-12-13 00:07:34 -0600

    temp

--- includes/fifo.h
+++ includes/fifo.h
@@ -7,8 +7,11 @@ private:
     int MAX_TRACKS;
     int MAX_BUFFER;
     int current_track;
+    int next_track;
     int total;
     int element;
 public:
     fifo(int, int, int);
+private:
+    int next_read_index();
 };
--- src/fifo.cpp
+++ src/fifo.cpp
@@ -9,10 +9,10 @@ fifo::fifo(int MAX_TRACKS, int MAX_BUFFER, int current_track){
     element = 0;
 }
 int fifo::next_read_index(){
-    for(int i = 0; i < read_buffer; ++i){
+    for(int i = 0; i < read_buffer.size(); ++i){
         int next_track = read_buffer.front();
         read_buffer.pop_front();
-        seek.push_back(abs(current_track-next_track));
+        //seek.push_back(abs(current_track-next_track));
         current_track=next_track;
         total += abs(current_track-next_track);
         elements++;
