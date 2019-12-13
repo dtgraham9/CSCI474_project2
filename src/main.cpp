acd1f986eb98b0234ac4074de8af2b61f9103c9d
Graham Thompson <graham.thompson@ndsu.edu>
2019-12-13 02:15:46 -0600

    random

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 #include <iostream>
 #include <time.h>
 
+int rand_gen(int prev_track, int edge){
+    return 0;
+}
+
+
 int main(int argc, char *argv[]){
-    int num_reads =1000, max_tracks=200;
+    int num_reads =1000, max_tracks=200, rand_tracks = 1000;
     std::vector<int> track_queue;
     srand(time(0));
 
@@ -12,5 +17,31 @@ int main(int argc, char *argv[]){
         track_queue.push_back(rand() % max_tracks);
     }
 
-    
+    std::vector<int> track_queue_wgt;
+    track_queue_wgt.push_back(rand() % max_tracks);
+    int hold_rand = 0;
+    for(int i = 1; i < rand_tracks; ++i){
+        hold_rand = (rand() % 100) +1 
+        if(hold_rand <= 10 && hold_rand >= 1){
+            track_queue_wgt.push_back(track_queue_wgt.back);
+            continue;
+        }
+        else if(hold_rand == 100){
+            if((rand()% 1) == 0){
+                track_queue_wgt.push_back(0);
+            }
+            else{
+                track_queue_wgt.push_back(max_tracks-1);
+            }
+        }
+        else{
+            if(rand() % 1 == 0){
+                track_queue_wgt.push_back(rand_gen(track_queue_wgt.back, 0));
+            }
+            else{
+                track_queue_wgt.push_back(rand_gen_for(track_queue_wgt.back, max_tracks-1));
+            }
+        }
+
+    }
 }
