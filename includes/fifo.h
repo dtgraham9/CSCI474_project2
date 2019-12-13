d0d767fd61256d354c5c35adabd927882d9112bb
Jacques Hilo <hilo@lab10.cs.ndsu.nodak.edu>
2019-12-13 00:05:53 -0600

    fix fifo 2

--- includes/fifo.h
+++ includes/fifo.h
@@ -9,6 +9,20 @@ private:
     int current_track;
     int total;
     int element;
+    int num_tracks_traversed;
+    std::ofstream scanfile;
+    int num_tracks_requested;
+    int avg_num_track;
+
 public:
     fifo(int, int, int);
+    bool full();
+    void read();
+    void add(int);
+    void add_tracks(std::vector<int> &);
+    int space_left(); 
+    void print_report();
+
+private:
+    int next_read_index();
 };
